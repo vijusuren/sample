8a8a34424c5fb9e22b0ea12801bad6854c62b278
vijusuren <vijayalakshmioviya@gmail.com>
2021-01-24 18:20:24 +0000

    update

--- spec/calcSpec.js
+++ spec/calcSpec.js
@@ -1,16 +1,23 @@
 describe("Calculator", function() {
+
+    var calc = new Calculator;
+
     describe("Addition function", function() {
         it("should return 42", function() {
-            expect(addition(20, 22)).toBe(42);
+            calc.add(20);
+            calc.add(22);
+            expect(calc.value).toBe(42);
         });
 
         it("should return 26", function() {
-            expect(addition(7, 19)).toBe(26);
+            calc.add(7);
+            calc.add(19);
+            expect(calc.value).toBe(26);
         });
 
         it("should return an error if we don't supply two numbers", function() {
             spyOn(window, "alert");
-            addition("Hitchhikers", "Guide")
+            calc.add("Hitchhikers");
             expect(window.alert).toHaveBeenCalledWith("Error!");
         });
     });
